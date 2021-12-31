bf5dd5ae1354f0fd07d6095057427e018b2b3282
Andre Kubotsu <contato201@gmail.com>
2021-12-31 10:56:19 -0300

    folders changed

--- pages/index.tsx
+++
@@ -1,9 +0,0 @@
-import type { NextPage } from "next";
-import Head from "next/head";
-import Image from "next/image";
-
-const Home: NextPage = () => {
-  return <h1>World Trip</h1>;
-};
-
-export default Home;
